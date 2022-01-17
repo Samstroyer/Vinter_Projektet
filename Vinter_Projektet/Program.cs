58556c522c10ae63777a4628cd928da0104f7eb5
Samuel Palmér <palmer.samuel@elev.ga.ntig.se>
2022-01-17 15:55:44 +0100

    Startat MapGen

--- Vinter_Projektet/Program.cs
+++ Vinter_Projektet/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Raylib_cs;
+using System.IO;
+using System.Linq;
 using System.Numerics;
 
 namespace VinterProjektet
@@ -19,7 +21,7 @@ namespace VinterProjektet
 
         static void GameLoop()
         {
-            string menu = "start";
+            string menu = "creator";
             while (!Raylib.WindowShouldClose())
             {
                 switch (menu)
@@ -27,10 +29,62 @@ namespace VinterProjektet
                     case "start":
                         menu = DisplayStartScreen();
                         break;
-                    default:
+                    case "creator":
+                        CreateMap();
+                        menu = Creator();
                         break;
+
+                        //default:
+                        //menu = DisplayStartScreen();
+                        //break;
                 }
             }
+        }
+
+        static void CreateMap()
+        {
+            //Index:
+            //wT = water tile : 0-10 - 91-100
+            //fT = flat tile : 11-20 - 46-55 - 81-90
+            //fT = forrest tile : 21-45
+            //sT = stone tile : 56-80
+
+            //Högre borde i teorin vara lättare men mer utspritt
+            int genDiff = 5;
+            Random gen = new Random();
+
+            if (File.Exists(@"SaveData\WorldData.txt"))
+            {
+                Console.WriteLine("existsMyGuy!");
+                string[] map = File.ReadAllLines(@"SaveData\WorldData.txt");
+            }
+            else
+            {
+                Tile[,] tiles = new Tile[200, 200];
+            }
+
+            while (!Raylib.WindowShouldClose())
+            {
+
+            }
+        }
+
+        static string Creator()
+        {
+            int[] tileSize = new int[12] { 5, 8, 10, 16, 20, 25, 32, 40, 50, 80, 100, 160 };
+
+
+
+            int currSize = tileSize[5];
+            bool playing = true;
+
+            while (playing && !Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.EndDrawing();
+            }
+
+            return "menu";
         }
 
         static string DisplayStartScreen()
@@ -39,7 +93,6 @@ namespace VinterProjektet
             int recHeight = 100;
             (Rectangle, string, string)[] buttons = new (Rectangle, string, string)[3] { (new Rectangle(), "Play", "play"), (new Rectangle(), "Create Map", "creator"), (new Rectangle(), "Settings", "settings") };
             string nextMenu = "";
-            bool selected = false;
 
             int iOffset = 150;
             int startY = 300;
@@ -50,7 +103,7 @@ namespace VinterProjektet
                 buttons[i].Item1 = new Rectangle(x, y, recWidth, recHeight);
             }
 
-            while (nextMenu == "")
+            while (nextMenu == "" && !Raylib.WindowShouldClose())
             {
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.WHITE);
@@ -61,8 +114,8 @@ namespace VinterProjektet
 
                     Raylib.DrawRectangleRec(tempRec, Color.GRAY);
                     Raylib.DrawRectangleLinesEx(tempRec, 5, Color.BLACK);
-                    nextMenu = Click(buttons);
                     Raylib.DrawText(buttons[i].Item2, (int)tempRec.x + 30, (int)tempRec.y + 30, 40, Color.ORANGE);
+                    nextMenu = Click(buttons);
                 }
 
                 Raylib.EndDrawing();
@@ -79,12 +132,14 @@ namespace VinterProjektet
                 Vector2 mouseCords = Raylib.GetMousePosition();
 
                 Rectangle tempRec = buttons[i].Item1;
-
                 (int xStart, int xStop, int yStart, int yStop) buttonHitbox = ((int)tempRec.x, (int)tempRec.x + (int)tempRec.width, (int)tempRec.y, (int)tempRec.y + (int)tempRec.height);
 
                 if (mouseCords.X > buttonHitbox.xStart && mouseCords.X < buttonHitbox.xStop && mouseCords.Y > buttonHitbox.yStart && mouseCords.Y < buttonHitbox.yStop)
                 {
                     Raylib.DrawRectangleRec(tempRec, Color.DARKGRAY);
+                    Raylib.DrawRectangleLinesEx(tempRec, 5, Color.DARKBROWN);
+                    Raylib.DrawText(buttons[i].Item2, (int)tempRec.x + 30, (int)tempRec.y + 30, 40, Color.GOLD);
+
                     if (Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON))
                     {
                         Console.WriteLine($"Returning {buttons[i].Item3}");
@@ -93,6 +148,22 @@ namespace VinterProjektet
                 }
             }
             return "";
+        }
+    }
+
+    class Tile
+    {
+        Vector2 cords = new Vector2();
+
+        Tile(int x, int y)
+        {
+            cords.X = x;
+            cords.Y = y;
+        }
+
+        void Branch()
+        {
+            
         }
     }
 }
